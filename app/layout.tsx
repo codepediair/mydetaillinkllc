ac3e314d586f7be608230ec7b67c328fb6884ccb
mahdi rezaii <mahdirezaii490@gmail.com>
2025-11-16 00:51:37 +0330

    change shadcn theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Barlow, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const barlowSans = Barlow({
+  
+  weight: ['400', '600', '900'],
   subsets: ["latin"],
 });
 
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${barlowSans.className} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
