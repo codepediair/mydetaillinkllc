6066b766c2bd265f22c683cdd4c26d0b0c43bbad
mahdi rezaii <mahdirezaii490@gmail.com>
2025-11-19 21:57:58 +0330

    change icons

---
+++ components/logoIcon.tsx
@@ -0,0 +1,15 @@
+export function LogoIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="25"
+      viewBox="0 0 25 25"
+    >
+      <path
+        d="M19.47,0h0C15.62,6.9,6.9,12.5,0,12.5H0c6.9,0,9.38,5.6,5.53,12.5h0C9.38,18.1,18.1,12.5,25,12.5h0C18.1,12.5,15.62,6.9,19.47,0Z"
+        fill="#fff212"
+      />
+    </svg>
+  );
+}
