11c9d430f7b2a20fad2e6e84d0187241f9680a1d
mahdi rezaii <mahdirezaii490@gmail.com>
2025-11-25 21:08:12 +0330

    fix login bug

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,15 @@
 import TaskPage from "@/components/tasks/page";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    return redirect("/login");
+  }
   return(
     <TaskPage/>
   );
